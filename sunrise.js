e3dd1d30172cc7e0263b9b5fa723fdf9e47dcbfa
jiho6693 <56946702+jiho6693@users.noreply.github.com>
2023-03-17 18:58:02 -0400

    1
    1

---
+++ sunrise.js
@@ -0,0 +1,10 @@
+
+let lat = "41.825226"; 
+let lon = "-71.418884";
+
+
+const url = 'https://api.sunrise-sunset.org/json?lat=' + lat +'&lon=' + lon + '&date=today';
+
+var data = new Date();
+
+console.log(data)
---
+++ time.js
@@ -0,0 +1,9 @@
+let today = new Date();
+var hours = ('0' + today.getHours()).slice(-2); 
+var min = today.getMinutes();
+
+console.log(today)
+console.log(hours)
+console.log(min)
+
+
